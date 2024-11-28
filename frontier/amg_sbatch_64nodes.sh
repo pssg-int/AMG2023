d5f7e0b37c985d1339f7c0ef3c0688a68e42b6ee
Keshav Pradeep <32313895+keshprad@users.noreply.github.com>
2024-11-28 00:09:10 -0500

    fix issue with gpu-bind impacting memory usage on gemm benchmark

--- frontier/amg_sbatch_64nodes.sh
+++ frontier/amg_sbatch_64nodes.sh
@@ -6,7 +6,6 @@
 #SBATCH -J amg
 #SBATCH -t 00:30:00
 #SBATCH -A csc569
-#SBATCH --gpu-bind=none
 #SBATCH --output /lustre/orion/csc569/scratch/keshprad/perfvar/AMG2023_logs/64nodes/%x-%j/output-AMG2023.log
 #SBATCH --error /lustre/orion/csc569/scratch/keshprad/perfvar/AMG2023_logs/64nodes/%x-%j/error-AMG2023.log
 # Run like: sbatch frontier/amg_sbatch_64nodes.sh
